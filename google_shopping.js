bd7d8611193aae4e45b3cf2eb298b00a0991a0a4
keidra <keidrabaldwin@gmail.com>
2016-03-23 18:26:11 -0700

    completed conditionals and loops HW

--- google_shopping.js
+++ google_shopping.js
@@ -1,7 +1,6 @@
 var data = require("./products.json")
 
-// Write your solutions below
-
+// Problem 1:
 var count = 0;
 for (var i = 0; i < data['items'].length; i++) {
 	if (data['items'][i].kind === 'shopping#product') {
@@ -9,3 +8,43 @@ for (var i = 0; i < data['items'].length; i++) {
 	}
 }
 console.log(count);
+// Problem 2:
+for (var i = 0; i < data['items'].length; i++) {
+  if (data.items[i].product.inventories[0].availability === 'backorder') {
+    console.log(data.items[i].product.title);
+  }
+}
+// Problem 3:
+for (var i = 0; i < data['items'].length; i++) {
+  if (data.items[i].product.images.length > 1) {
+    console.log(data.items[i].product.title);
+  }
+}
+// Problem 4:
+for (var i = 0; i < data['items'].length; i++) {
+  if (data.items[i].product.brand === 'Canon') {
+    console.log(data.items[i].product.title);
+  }
+}
+
+// Problem 5:
+for (var i = 0; i < data['items'].length; i++) {
+  if (data.items[i].product.brand === 'Canon' && data.items[i].product.author.name[0].indexOf("eBay")){
+    console.log(data.items[i].product.title);
+  }
+}
+
+// Problem 6:
+var brand;
+var price;
+var image;
+
+for (var i = 0; i < data['items'].length; i++) {
+  {
+    console.log(data.items[i].product.title + ' WITH A BRAND OF ' + data.items[i].product.brand + ' WITH A PRICE OF $' + data.items[i].product.inventories[0].price + '.' + "HERE'S A LINK: " + data.items[i].product.images[0].link) ;
+  }
+}
+
+
+
+
